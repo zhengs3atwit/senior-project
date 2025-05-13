e8bb464f820efc46619d211ad874c65df3bb4785
Steven Zheng <zhengs3@wit.edu>
2025-05-13 17:08:36 -0400

    Update urls.py

--- project/home/urls.py
+++ project/home/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('malware', views.malware, name='malware'),
-    path('credential', views.credential, name='credential'),
-    path('spear', views.spear, name='spear'),
-    path('fakelogin', views.fakelogin, name='fakelogin'),
+    path('campaigns/malware/', views.malware, name='malware'),
+    path('campaigns/credential/', views.credential, name='credential'),
+    path('campaigns/spear/', views.spear, name='spear'),
+    path('campaigns/fakelogin/', views.fakelogin, name='fakelogin'),
 ]
